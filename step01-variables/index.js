f336f0a1e77801fc904d53a193d094e9a0633c75
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-03-25 21:46:58 +0500

    Variables

---
+++ step01-variables/index.js
@@ -0,0 +1,6 @@
+// Variables.
+let favoriteFruit = "Mango"; //This will store your favorite fruit/data in variable.
+console.log(favoriteFruit);
+favoriteFruit = "Kiwi"; //This will change the value of your favorite fruit/data.
+console.log(favoriteFruit);
+export {};
---
+++ step01-variables/index.ts
@@ -0,0 +1,6 @@
+// Variables.
+let favoriteFruit:string = "Mango";//This will store your favorite fruit/data in variable.
+console.log(favoriteFruit);
+
+favoriteFruit = "Kiwi";//This will change the value of your favorite fruit/data.
+console.log(favoriteFruit);
