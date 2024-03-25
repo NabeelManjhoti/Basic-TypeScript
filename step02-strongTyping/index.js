a16d7b96c6c1171b300b3d4e945e3d6834eb48dd
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-03-25 23:34:48 +0500

    Strong Typing

---
+++ step02-strongTyping/index.js
@@ -0,0 +1,9 @@
+// Strong Typing in TypeScript.
+let userName = "Nabeel Ali"; //This is strongly typed string.
+export {};
+// userName = 5;
+/*
+ Attempting to assign a number to userName would result error.
+ This is TypeScript enforcing strong typing,
+ ensuring that variables are always used with the correct type of data.
+*/
---
+++ step02-strongTyping/index.ts
@@ -0,0 +1,10 @@
+// Strong Typing in TypeScript.
+let userName:string = "Nabeel Ali";//This is strongly typed string.
+
+// userName = 5;
+/* 
+ Attempting to assign a number to userName would result error.
+ This is TypeScript enforcing strong typing,
+ ensuring that variables are always used with the correct type of data.
+*/
+
