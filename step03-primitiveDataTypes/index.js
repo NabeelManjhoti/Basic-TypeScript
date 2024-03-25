e359e6de4b815c0b124a4eadbc489cdf0458cec1
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-03-25 23:57:28 +0500

    Data Types

---
+++ step03-primitiveDataTypes/index.js
@@ -0,0 +1,13 @@
+// Primitive Data Types.
+// Here are some basic Data types in TypeScript.
+/*
+  String: For textual Data.
+  Number: For numerical values (integers and floting-point numbers).
+  Boolean: For True/False values.
+  Any: For variables that can hold any type of Data.
+*/
+let userName = "Nabeel Ali";
+let age = 26;
+let isGenAiStudent = true;
+let mobileNumber = "031234567890";
+export {};
---
+++ step03-primitiveDataTypes/index.ts
@@ -0,0 +1,16 @@
+// Primitive Data Types.
+
+// Here are some basic Data types in TypeScript.
+/*
+  String: For textual Data.
+  Number: For numerical values (integers and floting-point numbers).
+  Boolean: For True/False values.
+  Any: For variables that can hold any type of Data.
+*/
+
+let userName:string = "Nabeel Ali";
+let age:number = 26;
+let isGenAiStudent:boolean = true;
+let mobileNumber:any = "031234567890";
+
+
